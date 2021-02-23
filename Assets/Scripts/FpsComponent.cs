12346c94419bd7b5ae5c8e02c39830dd37304712
Oleksii Voronchuk <56025008+oleksii-voronchuk@users.noreply.github.com>
2021-02-23 17:37:59 +0200

    Sample scene added
    A new project has been created in Unity 2020.2.5f1. Added a test model of a ship, FPS counter, the camera setup, etc.

---
+++ Assets/Scripts/FpsComponent.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class FpsComponent : MonoBehaviour
+    {
+        [SerializeField]
+#pragma warning disable 649
+        private Text displayText;
+#pragma warning restore 649
+        [SerializeField]
+        private int frameRange;
+
+        private int[] fpsBuffer;
+        private int fpsBufferIndex;
+        private int averageFPS;
+
+        private void Start()
+        {
+            InitializeBuffer();
+        }
+
+        private void Update()
+        {
+            UpdateBuffer();
+            CalculateFps();
+            UpdateFpsView();
+        }
+
+        private void InitializeBuffer()
+        {
+            if (frameRange <= 0)
+            {
+                frameRange = 1;
+            }
+
+            fpsBuffer = new int[frameRange];
+            fpsBufferIndex = 0;
+        }
+
+        private void UpdateBuffer()
+        {
+            fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+            if (fpsBufferIndex >= frameRange)
+            {
+                fpsBufferIndex = 0;
+            }
+        }
+
+        private void CalculateFps()
+        {
+            var sum = 0;
+            var lowest = int.MaxValue;
+            var highest = 0;
+            for (var i = 0; i < frameRange; i++)
+            {
+                var fps = fpsBuffer[i];
+                sum += fps;
+                if (fps > highest)
+                {
+                    highest = fps;
+                }
+
+                if (fps < lowest)
+                {
+                    lowest = fps;
+                }
+            }
+
+            averageFPS = sum / frameRange;
+        }
+
+        private void UpdateFpsView()
+        {
+            displayText.text = averageFPS.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/RotationComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RotationComponent : MonoBehaviour
+    {
+#pragma warning disable 649
+        [SerializeField] private float speed;
+#pragma warning restore 649
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * speed);
+        }
+    }
+}
